3dcc9e418dccfd37405513701fa5bcbe1e53f99c
Egor <egorkak1408@gmail.com>
2025-08-09 19:55:09 +0300

    Добавление пользователя в базу данных

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    role_master = BooleanField('Я мастер')
+    submit = SubmitField('Зарегистрироваться')
--- app/routers.py
+++ app/routers.py
@@ -1,11 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
+from app.forms import RegistrationForm
 from .extensions import db
+from .models.user import User
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/registration')
-def create_user():
-    return render_template('create-character.html')
+@app.route('/registration', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, role_master=form.role_master.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Регистрация прошла успешно!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create-character.html', form=form)
